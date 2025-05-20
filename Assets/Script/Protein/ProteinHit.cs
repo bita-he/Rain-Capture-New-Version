a423751b580a5c5fca47244d4449dbbf5c4ef13e
bita-he <bitaheydari0625@gmail.com>
2025-05-20 21:29:21 +0330

    ADD: Protein Col detector

--- Assets/Script/Protein/ProteinHit.cs
+++ Assets/Script/Protein/ProteinHit.cs
@@ -5,19 +5,20 @@ namespace Script.Protein
 {
     public class ProteinHit : MonoBehaviour
     {
-        void Start()
-        {
-        
-        }
-
-        void Update()
-        {
-        
-        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            
+            if (other.CompareTag("Player"))
+            {
+                //Increase score +10
+                Destroy(gameObject);
+            }
+
+            if (other.CompareTag("Ground"))
+            {
+                //Decrease score -5
+                Destroy(gameObject);
+            }
         }
     }
 }
